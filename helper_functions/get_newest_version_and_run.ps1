53fd8fcefdea8a2c6a96f07951dc8baaf72d6ba5
mikah <mspinega@ncsu.edu>
2021-07-26 10:17:14 -0400

    add newline

--- helper_functions/get_newest_version_and_run.ps1
+++ helper_functions/get_newest_version_and_run.ps1
@@ -2,6 +2,6 @@ Set-Location C:\Users\mikah\Documents\etl-forms
 git checkout master 
 git pull > logs\tempfile.txt
 python parse_forms\parse_forms.py >> logs\tempfile.txt
-# Write-Output '\n' >> logs\tempfile.txt
+Write-Output "`n" >> logs\tempfile.txt
 Get-Content logs\logs.txt >> logs\tempfile.txt
 Copy-Item logs\tempfile.txt logs\logs.txt
