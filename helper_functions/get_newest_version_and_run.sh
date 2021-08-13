1e7e51f750106f9935d282e473df5f953202a5bb
Ubuntu <azureuser@KoboForms.nptcppinri1uzhjob4fhoyd2hc.phx.internal.usgovcloudapp.net>
2021-08-13 16:37:28 +0000

    update shell

--- helper_functions/get_newest_version_and_run.sh
+++ helper_functions/get_newest_version_and_run.sh
@@ -4,12 +4,10 @@
 cd /home/azureuser/etl-forms
 git checkout master 
 git pull > logs/tempfile.log
-python3 parse_forms/parse_forms.py >> logs/tempfile.log
+python3 run_live.py >> logs/tempfile.log
 echo '\n' >> logs/tempfile.log
 
-# run and log push_to_prod.py
-python3 push_to_prod/push_to_prod.py >> logs/tempfile.log
-echo '\n' >> logs/tempfile.log
-cat logs/logs.log >> logs/tempfile.log
-cp logs/tempfile.log logs/logs.log
+# prepend to master.log
+cat logs/master.log >> logs/tempfile.log
+cp logs/tempfile.log logs/master.log
 
